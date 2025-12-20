983f274335852436b1c019bfa1bda85744de6e7c
Santhru <santhru2811@gmail.com>
2025-12-20 09:33:26 +0530

    Spin game  added

--- Todo-list.py
+++ Todo-list.py
@@ -10,9 +10,9 @@ def remove_task(todo):
     if not todo:
         print("No tasks in the list.")
         return
-    task = input("Enter the task to remove: ")
-    if task in todo:
-        todo.remove(todo[todo.index(task)])
+    task = int(input("Enter the task to remove: "))
+    if task <= len(todo) and task > 0:
+        del todo[task-1]
         print(f'Task "{task}" removed from the list.')
     else:
         print(f'Task "{task}" not found in the list.')
@@ -26,19 +26,19 @@ def view_tasks(todo):
             print(f"{i}. {task}")
 
 def todo_list():
-    todo = []
+    todo = []   
     while True:
         func_name = input(" 1.Add Task\n 2.Remove Task\n 3.View Tasks\n 4.Quit.\n Enter function name: ").lower()
-        if func_name == 'Add Task'.lower():
+        if func_name == '1' or func_name == 'Add Task'.lower():
             add_task(todo)
-        elif func_name == 'Remove Task'.lower():
+        elif func_name == '2' or func_name == 'Remove Task'.lower():
             remove_task(todo)
-        elif func_name == 'View Tasks'.lower():
+        elif func_name == '3' or func_name == 'View Tasks'.lower():
             view_tasks(todo)
-        elif func_name == 'quit':
+        elif func_name == '4' or func_name == 'Quit'.lower():
             print("Exiting the todo list.")
             break
         else:
-            print("Invalid option. Please choose add_task, remove_task, view_tasks, or quit.")
+            print("Invalid option. Please choose 1, 2, 3, or 4.")
 
 todo_list()
